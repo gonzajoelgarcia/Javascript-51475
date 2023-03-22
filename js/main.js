bed035480e6fc62d6640fe2733d48365117e1ae0
Gonzalo García <gonzajoelgarcia.gg@gmail.com>
2023-03-22 05:18:25 -0300

    TindaOnline-García

--- js/main.js
+++ js/main.js
@@ -1,97 +1,226 @@
-function prestamo_solicitado(sueldo, prestamo) {
-  if (sueldo > prestamo) {
-    alert("Usted puede obtener el préstamo solicitado.");
-    return true;
-  } else {
-    alert("Usted no puede solicitar un préstamo mayor a su sueldo.");
-    return false;
-  }
-}
+//! productosarray
 
-let sueldo = Number(prompt("Ingrese su sueldo mensual"));
-
-while (isNaN(sueldo) || sueldo <= 0) {
-  sueldo = Number(
-    prompt("El sueldo debe ser mayor que cero. Ingrese un sueldo válido.")
-  );
-}
-
-let prestamo = Number(prompt("Ingrese el monto del préstamo que solicita."));
-
-while (isNaN(prestamo) || prestamo <= 0) {
-  prestamo = Number(
-    prompt("El préstamo debe ser mayor que cero. Ingrese un préstamo válido.")
-  );
-}
-
-let resultado = prestamo_solicitado(sueldo, prestamo);
-
-if (resultado == true) {
-  const iva = 1.1;
-
-  const interes = prestamo * iva;
-
-  const prestamo_final = prestamo + interes;
-
-  alert(
-    "El monto final que deberá pagar con intereses del 10% es de: " +
-      prestamo_final +
-      " .A continuacion elija el destino o finalidad del prestamo:"
-  );
-}
-
-function prestamoTotal(arr) {
-  let resultado = 0;
-  arr.forEach((prestamo) => {
-    resultado += prestamo.total / prestamo.cuotasPagar;
-  });
-  return resultado;
-}
-
-const resultadoPrestamo = [];
-const listaPrestamos = [
-  { id: 1, prestamo: "personales.", total: 1000 },
-  { id: 2, prestamo: "al consumo.", total: 2500 },
-  { id: 3, prestamo: "de estudios.", total: 5500 },
-  { id: 4, prestamo: "hipotecarios.", total: 8000 },
-  { id: 5, prestamo: "para empresas.", total: 10000 },
+const productos = [
+  //! SUDADERAS
+  {
+    id: "sudadera-01",
+    titulo: "Sudadera LA",
+    imagen: "../img/sudadera-01.jpg",
+    categoria: {
+      nombre: "Sudaderas",
+      id: "sudaderas",
+    },
+    precio: 3960,
+  },
+  {
+    id: "sudadera-02",
+    titulo: "Sudadera Positive",
+    imagen: "../img/sudadera-02.jpg",
+    categoria: {
+      nombre: "Sudaderas",
+      id: "sudaderas",
+    },
+    precio: 3520,
+  },
+  {
+    id: "sudadera-03",
+    titulo: "Sudadera Tipografica",
+    imagen: "../img/sudadera-03.jpg",
+    categoria: {
+      nombre: "Sudaderas",
+      id: "sudaderas",
+    },
+    precio: 3520,
+  },
+  {
+    id: "sudadera-04",
+    titulo: "Sudadera Brooklyn",
+    imagen: "../img/sudadera-04.jpg",
+    categoria: {
+      nombre: "Sudaderas",
+      id: "sudaderas",
+    },
+    precio: 4620,
+  },
+  //! CAMISETAS
+  {
+    id: "camiseta-01",
+    titulo: "Camiseta CHANGE",
+    imagen: "../img/camiseta-01.jpg",
+    categoria: {
+      nombre: "Camisetas",
+      id: "camisetas",
+    },
+    precio: 2860,
+  },
+  {
+    id: "camiseta-02",
+    titulo: "Camiseta Joystick",
+    imagen: "../img/camiseta-02.jpg",
+    categoria: {
+      nombre: "Camisetas",
+      id: "camisetas",
+    },
+    precio: 2640,
+  },
+  {
+    id: "camiseta-03",
+    titulo: "Camiseta GROUND",
+    imagen: "../img/camiseta-03.jpg",
+    categoria: {
+      nombre: "Camisetas",
+      id: "camisetas",
+    },
+    precio: 2640,
+  },
+  {
+    id: "camiseta-04",
+    titulo: "Camiseta Palms",
+    imagen: "../img/camiseta-04.jpg",
+    categoria: {
+      nombre: "Camisetas",
+      id: "camisetas",
+    },
+    precio: 3300,
+  },
+  //! PANTALONES
+  {
+    id: "pantalones-01",
+    titulo: "Pantalones Blue",
+    imagen: "../img/pantalones-01.jpg",
+    categoria: {
+      nombre: "Pantalones",
+      id: "pantalones",
+    },
+    precio: 7920,
+  },
+  {
+    id: "pantalones-02",
+    titulo: "Pantalones JEAN",
+    imagen: "../img/pantalones-02.jpg",
+    categoria: {
+      nombre: "Pantalones",
+      id: "pantalones",
+    },
+    precio: 8360,
+  },
+  {
+    id: "pantalones-03",
+    titulo: "Pantalones Yellow",
+    imagen: "../img/pantalones-03.jpg",
+    categoria: {
+      nombre: "Pantalones",
+      id: "pantalones",
+    },
+    precio: 8360,
+  },
+  {
+    id: "pantalones-04",
+    titulo: "Pantalones 0DD",
+    imagen: "../img/pantalones-04.jpg",
+    categoria: {
+      nombre: "Pantalones",
+      id: "pantalones",
+    },
+    precio: 7700,
+  },
 ];
 
-let visualizarPrestamos = "";
-listaPrestamos.forEach((prestamo) => {
-  visualizarPrestamos +=
-    "id: " +
-    prestamo.id +
-    " préstamo: " +
-    prestamo.prestamo +
-    " total: $" +
-    prestamo.total +
-    "\n";
-});
+//! DOM
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
 
-alert(visualizarPrestamos);
+function cargarProductos(productosElegidos) {
+  contenedorProductos.innerHTML = "";
+  productosElegidos.forEach((producto) => {
+    const div = document.createElement("div");
+    div.classList.add("producto");
+    div.innerHTML = `
+        <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+                    <div class="producto-detalles">
+                        <h3 class="producto-titulo">${producto.titulo}</h3>
+                        <p class="producto-precio">${producto.precio}</p>
+                        <button class="producto-agregar" id="${producto.id}">Agregar</button>
+                    </div>
+        `;
 
-do {
-  listaPrestamos.forEach((prestamo) => {
-    console.log(prestamo.id);
-    console.log(prestamo.prestamo);
-    console.log(prestamo.total);
+    contenedorProductos.append(div);
   });
 
-  let id = prompt("Ingrese el ID del prestamo que solicita", 1);
-  if (!isNaN(id))
-    if (listaPrestamos.some((prestamo) => prestamo.id == id)) {
-      let cuotasPagar = prompt("¿En cuántas cuotas quiere pagarlo?");
-      const prestamo = listaPrestamos.find((prestamo) => prestamo.id == id);
-      prestamo.cuotasPagar = cuotasPagar;
-      resultadoPrestamo.push(prestamo);
+  actualizarBotonesAgregar();
+}
+
+cargarProductos(productos);
+
+botonesCategorias.forEach((boton) => {
+  boton.addEventListener("click", (e) => {
+    botonesCategorias.forEach((boton) => boton.classList.remove("active"));
+    e.currentTarget.classList.add("active");
+
+    if (e.currentTarget.id != "todos") {
+      const productoCategoria = productos.find(
+        (producto) => producto.categoria.id === e.currentTarget.id
+      );
+      tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+      const productosBoton = productos.filter(
+        (producto) => producto.categoria.id === e.currentTarget.id
+      );
+      cargarProductos(productosBoton);
     } else {
-      console.log("Ingresó un ID inexistente.");
+      tituloPrincipal.innerText = "Todos los productos";
+      cargarProductos(productos);
     }
+  });
+});
 
-  rta = prompt("¿Desea finalizar su solicitud del préstamo? SI/NO");
-} while (rta != "si");
+function actualizarBotonesAgregar() {
+  botonesAgregar = document.querySelectorAll(".producto-agregar");
 
-alert(
-  "El monto total a pagar por mes será de: $" + prestamoTotal(resultadoPrestamo)
-);
+  botonesAgregar.forEach((boton) => {
+    boton.addEventListener("click", agregarAlCarrito);
+  });
+}
+
+let productosEnCarrito;
+
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+  productosEnCarrito = JSON.parse(productosEnCarritoLS);
+  actualizarNumerito();
+} else {
+  productosEnCarrito = [];
+}
+
+function agregarAlCarrito(e) {
+  const idBoton = e.currentTarget.id;
+  const productoAgregado = productos.find(
+    (producto) => producto.id === idBoton
+  );
+  if (productosEnCarrito.some((producto) => producto.id === idBoton)) {
+    const index = productosEnCarrito.findIndex(
+      (producto) => producto.id === idBoton
+    );
+    productosEnCarrito[index].cantidad++;
+  } else {
+    productoAgregado.cantidad = 1;
+    productosEnCarrito.push(productoAgregado);
+  }
+  actualizarNumerito();
+
+  localStorage.setItem(
+    "productos-en-carrito",
+    JSON.stringify(productosEnCarrito)
+  );
+}
+
+function actualizarNumerito() {
+  let nuevoNumerito = productosEnCarrito.reduce(
+    (acc, producto) => acc + producto.cantidad,
+    0
+  );
+  numerito.innerText = nuevoNumerito;
+}
