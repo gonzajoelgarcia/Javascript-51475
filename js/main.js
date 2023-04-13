c54ad0a69e6fcc37cc46be9cad921bce5d1040e8
Gonzalo García <gonzajoelgarcia.gg@gmail.com>
2023-04-12 23:40:25 -0300

    fixed-bugs-6l

--- js/main.js
+++ js/main.js
@@ -41,6 +41,7 @@ botonesCategorias.forEach((boton) => {
   boton.addEventListener("click", (e) => {
     botonesCategorias.forEach((boton) => boton.classList.remove("active"));
     e.currentTarget.classList.add("active");
+
     if (e.currentTarget.id != "todos") {
       const productoCategoria = productos.find(
         (producto) => producto.categoria.id === e.currentTarget.id
